73dd444c86cca88b86290232cacf511797b6732e
hailemedn <hailemedhin.k@gmail.com>
2024-01-20 18:11:46 +0300

    add the first list from an array

--- 33 - Todolist - onMyOwnAttempt/app.js
+++ 33 - Todolist - onMyOwnAttempt/app.js
@@ -5,13 +5,16 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+const lists = ["Welcome to 2Do", "Enter your tasks below", "<-- Click here to delete"]
 
 app.get("/", (req, res) => {
-    res.render("list", {listTitle: "Today"});
-})
-
+    res.render("list", {
+        listTitle: "Today",
+        lists: lists});
+});
 
 app.listen("3000", () => {
     console.log("Server started at port 3000!");
-})
+});
