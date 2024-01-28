b922bab25d45ba264dc141557441eccbf5a255a6
hailemedn <hailemedhin.k@gmail.com>
2024-01-28 16:05:22 +0300

    learn Basics of typescript

--- TypeScript/typescript-tutorial/src/App.tsx
+++ TypeScript/typescript-tutorial/src/App.tsx
@@ -1,24 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let id: any; //not recommended
+  let identificaton: unknown;
+  let name: string;
+  let age: number;
+  let tag: string | number; //union, takes string or number;
+  let isLegal: boolean;
+  let hobbies: string[];
+  let scores: number[];
+  let role: [number, string]
+
+  // type Person = {
+  //   name: string;
+  //   age?: number;
+  // }
+
+  interface Person {
+    name: string,
+    age?: number,
+  };
+
+  let person: Person = {
+    name: "Haile"
+  };
+
+  let lotsOfPeople: Person[];
+
+  lotsOfPeople = [
+   {name: "Haile"},
+   {name: "Segio", age: 36}
+  ];
+
+  let printName: (name: string) => void; //output type unknown yet
+  let someMethod: (name: string) => number;
+  let anotherFunction: (age: number) => never; //outputs nothing
+
+
+  //extending properties in alices
+  //type
+  type X = {
+    a: string;
+    b: number;
+  }
+
+  type Y = X & {
+    c: string;
+    d: number;
+  }
+
+  let y: Y = {
+    a: "ere",
+    b: 32,
+    c: "hmm",
+    d: 12
+  }
+
+
+  //interfaces
+  interface student extends Person {
+    grade: string;
+  }
+
+  let gukesh: student = {
+    name: 'Gukesh',
+    age: 17,
+    grade: 'A',
+  }
+
+  //extending property inside an interface to a type
+  type N = Person & {
+    a:string;
+    b:number;
+  }
+
+  let someOne:N = {
+    name: "someone",
+    a: "a",
+    b: 12,
+  }
+
+  //extending property inside type to an interface
+  interface M extends N {
+    c: string;
+    d: number;
+  }
+
+  let anotherSomeOne:M = {
+    name: "anotherSomeOne",
+    a: "try",
+    b: 12,
+    c: "again",
+    d: 21
+  }
+
+  console.log(lotsOfPeople);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
